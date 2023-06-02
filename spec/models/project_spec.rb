5fdb39a9546753f295860daf2501091586e4484d
kakeru Aoyama <kakeru0520sou1@gmail.com>
2023-06-02 18:00:39 +0900

    project_specの作成

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  it "does not allow duplidate project name per user" do
+    user = User.create(
+      first_name: "Joe",
+      last_name:  "Tster",
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    user.projects.create(
+      name: "Test Project"
+    )
+
+    new_project = user.projects.build(
+      name: "Test Project"
+    )
+
+    new_project.valid?
+    expect(new_project.errors[:name]).to include("has already been taken")  
+  end
+
+  it "allow two users to share a project name" do
+    user = User.create(
+      first_name: "Joe",
+      last_name:  "Tster",
+      email:      "joetester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    user.projects.create(
+      name: "Test Project"
+    )
+
+    other_user = User.create(
+      first_name: "Jane",
+      last_name:  "Tster",
+      email:      "janetester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    other_project = other_user.projects.create(
+      name: "Test Project"
+    )
+
+    expect(other_project).to be_valid
+  end
+end
