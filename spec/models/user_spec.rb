d0971e49f71fa31512836353d5645b0a454ed3f6
kakeru Aoyama <kakeru0520sou1@gmail.com>
2023-06-06 14:54:45 +0900

    FactoryBotの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "has a valid factory" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
   it "is valid with a first name, last name and email, and password" do
     user = User.new(
       first_name: "Aaron",
